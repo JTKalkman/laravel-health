86d9b5ba14ff9cf7418cf92d5a3de7492c7aabce
J.T. Kalkman <mail@jtkalkman.nl>
2026-05-04 23:13:22 +0200

    Add availability checks.

--- src/HealthChecks/HealthCheck.php
+++ src/HealthChecks/HealthCheck.php
@@ -8,10 +8,32 @@ abstract class HealthCheck
 {
     protected string $name = 'Health check';
 
+    protected function canExec(): bool
+    {
+        if (ini_get('safe_mode')) return false;
+
+        $disabled = array_map('trim', explode(',', ini_get('disable_functions')));
+
+        return function_exists('exec') && !in_array('exec', $disabled);
+    }
+
+    protected function isAvailable(): bool
+    {
+        return true;
+    }
+
     abstract protected function performHealthCheck(): HealthCheckResult;
 
     public function run(): HealthCheckResult
     {
+        if (!$this->isAvailable()) {
+            return new HealthCheckResult(
+                name: $this->name,
+                status: 'error',
+                description: 'Check not available on this system',
+            );
+        }
+
         try {
             return $this->performHealthCheck();
         } catch (\Throwable $th) {
