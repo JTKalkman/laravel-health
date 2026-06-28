855d521807684570627b5a73884d877f6e3fdfdb
J.T. Kalkman <mail@jtkalkman.nl>
2026-06-28 11:18:14 +0200

    Add method to determine worst status.

--- src/HealthCheckStatus.php
+++ src/HealthCheckStatus.php
@@ -7,4 +7,20 @@ enum HealthCheckStatus: string
     case OK = 'ok';
     case WARNING = 'warning';
     case ERROR = 'error';
+
+    public function priority(): int
+    {
+        return match ($this) {
+            self::OK      => 0,
+            self::WARNING => 1,
+            self::ERROR   => 2,
+        };
+    }
+
+    public function worst(HealthCheckStatus $other): HealthCheckStatus
+    {
+        return $other->priority() > $this->priority()
+            ? $other
+            : $this;
+    }
 }
